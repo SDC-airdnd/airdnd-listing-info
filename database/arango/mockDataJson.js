77e23e4b2a149e497c7a1a11835bfe45d51080c3
Daniel Co <dcohomemail@gmail.com>
2020-08-31 16:20:12 -0700

    create script to generate data in jsonl format

--- database/arango/mockDataJson.js
+++ database/arango/mockDataJson.js
@@ -1,5 +1,6 @@
 const faker = require('faker');
-const mockCities = require('../mockCities.js');
+const mockCities = require('./mockCities');
+const mockUrls = require('./mockUrl');
 //const db = require('./index.js');
 
 let titleRandom = ['Perfectly located', 'Light & spacious garden flat', 'Private Modern Guesthouse', 'Ocean View Hideaway', 'Perfect Haven by Golden Gate', 'Private Backyard Cottage', 'Sunny Room Heart of', 'Luxury Gold Coast', 'Central Surfers Studio OceanView', 'Broken Head Bodhi Treehouse', 'Mountain tiny house', 'Blue Mountains Cottage', 'The Copa Cabana', 'The Tree House', 'Stroll Around Victoria Park', 'Entire Home with Opera House views', 'Luxury Apartment in the heart of', 'Stylish inner-city home', 'Little Paradise', 'Stunning River View' ]; 
@@ -13,7 +14,6 @@ function booleanGenerator() {
 function generateListing() {
     let titleRandomArray = titleRandom[Math.floor(Math.random()*titleRandom.length)];
     let roomInfoRandomArray = roomInfoRandom[Math.floor(Math.random()*roomInfoRandom.length)];
-    let hostImage = Math.floor(Math.random() * 1000) + 1;
 
     function numberOfGuests() {
         if (roomInfoRandomArray === 'Private room') {
@@ -46,13 +46,14 @@ function generateListing() {
             return Math.floor(Math.random()*(4-2)) + 2;
         }
     }
+
     var bedrooms = numberOfBedrooms();
-    let index = Math.floor(Math.random() * 1000) + 1;
-    let city = mockCities[index];
+    let city = mockCities[Math.floor(Math.random() * 1000)];
+    let hostImage = mockUrls[Math.floor(Math.random() * 1000)];
     let listing = {
-        cityId: index,
+        city: city,
         title: `${titleRandomArray} ${city}`,
-        hostImageId: hostImage,
+        hostImage: hostImage,
         roomInfo: roomInfoRandomArray,
         numberOfGuests: numberOfGuests(),
         numberOfBedrooms: bedrooms,
